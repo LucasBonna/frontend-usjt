0cc7345fd1e0366618d33d818246152f72a16992
Lucas Bonna Favaro <88051985+LucasBonna@users.noreply.github.com>
2024-06-09 18:57:50 -0300

    feat(lucas_bonna): added userInfo

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,60 @@
 "use client";
 
-import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
-import Cookies from 'js-cookie';
+import { api } from "@/lib/api";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default function Dashboard () {
-    const router = useRouter();
-    useEffect(() => {
-        const token = Cookies.get('token');
-        if (!token) {
-          router.push('/login');
-        }
-      }, [router]);
-    
-    return(
-        <div>Dashboard</div>
-    )
+interface UserData {
+  name: string;
+  email: string;
+}
+
+export default function Dashboard() {
+  const router = useRouter();
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (!token) {
+      router.push("/login");
+    } else {
+      fetchUserInfo(token);
+    }
+  }, [router]);
+
+  const fetchUserInfo = async (token: string) => {
+    try {
+      const response = await api.get<UserData>("api/v1/users/info", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUserData(response.data);
+    } catch (error) {
+      setError(error as Error);
+      console.log(error);
+      // router.push("/logout");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) return <div>Carregando...</div>;
+  if (error) return null;
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div>
+        <h2>Informações do Usuário</h2>
+        {userData && (
+          <pre>{JSON.stringify(userData, null, 2)}</pre>
+        )}
+      </div>
+      {/* Adicione outros componentes e conteúdo do dashboard aqui */}
+    </div>
+  );
 }
